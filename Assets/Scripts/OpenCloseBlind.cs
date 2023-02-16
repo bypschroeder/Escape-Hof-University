540d1c4890f6d9e22820cb234dbf9dc3230c625b
bypschroeder <patrick.schroeder@hof-university.de>
2023-02-16 12:06:17 +0100

    Added Features

--- Assets/Scripts/OpenCloseBlind.cs
+++ Assets/Scripts/OpenCloseBlind.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class OpenCloseBlind : MonoBehaviour
 {
     private Animator animator;
+    public GameObject switchDownObject;
+    public GameObject switchUpObject;
+    public Vector3 switchDownRotation;
+    public Vector3 switchUpRotation;
     public AudioSource openSound;
+    public AudioSource switchSoundOn;
+    public AudioSource switchSoundOff;
     private bool isOpen = true;
 
     void Start() {
@@ -13,7 +19,8 @@ public class OpenCloseBlind : MonoBehaviour
     }
 
     public void Open() {
-        if (!isOpen) {
+        if (!isOpen && AnimationIsFinished()) {
+            switchUpObject.transform.Rotate(switchDownRotation.x, switchDownRotation.y, switchDownRotation.z);
             // openSound.Play();
             isOpen = true;
             animator.SetTrigger("TrUp");
@@ -23,12 +30,17 @@ public class OpenCloseBlind : MonoBehaviour
     }
 
     public void Close() {
-        if (isOpen) {
+        if (isOpen && AnimationIsFinished()) {
+            switchDownObject.transform.Rotate(switchUpRotation.x, switchUpRotation.y, switchUpRotation.z);
             // openSound.Play();
             isOpen = false;
             animator.SetTrigger("TrDown");
         } else {
             return;
         }
+    }
+
+    bool AnimationIsFinished() {
+        return animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1;
     }
 }
--- Assets/Scripts/ToggleLight.cs
+++ Assets/Scripts/ToggleLight.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class ToggleLight : MonoBehaviour
 {
-    private Light light;
+    public Light spotLight;
+    public GameObject switchObject;
+    public Vector3 switchRotation;
     public GameObject lightObject;
     public Material onMaterial;
     public Material offMaterial;
-    [HideInInspector]
-    public bool isOn = true;
-
-    void Start()
-    {
-        light = GetComponent<Light>();
-    }
+    public AudioSource onSound;
+    public AudioSource offSound;
+    public bool isOn = false;
 
     public void Toggle() {
         if (isOn) {
-            light.enabled = false;
+            spotLight.enabled = false;
             lightObject.GetComponent<Renderer>().material = offMaterial;
+            switchObject.transform.Rotate(-switchRotation.x, -switchRotation.y, -switchRotation.z);
+            offSound.Play();
             isOn = false;
         } else {
-            light.enabled = true;
+            spotLight.enabled = true;
             lightObject.GetComponent<Renderer>().material = onMaterial;
+            switchObject.transform.Rotate(switchRotation.x, switchRotation.y, switchRotation.z);
+            onSound.Play();
             isOn = true;
         }
     }
